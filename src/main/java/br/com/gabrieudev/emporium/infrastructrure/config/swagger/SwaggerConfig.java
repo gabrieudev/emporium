5f86fcc498d737a036b33b8ef87792360b30644b
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-01-19 16:04:26 -0300

    chore: add production server

--- src/main/java/br/com/gabrieudev/emporium/infrastructrure/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/gabrieudev/emporium/infrastructrure/config/swagger/SwaggerConfig.java
@@ -1,30 +1,39 @@
 package br.com.gabrieudev.emporium.infrastructrure.config.swagger;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    OpenAPI emporiumOpenAPI(){
-        return new OpenAPI()
-                .info(new Info().title("API Emporium")
-                        .description("API do projeto Emporium")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Repositório do projeto")
-                        .url("https://github.com/gabrieudev/emporium/"));
-    }
+        @Bean
+        OpenAPI emporiumOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info().title("API Emporium")
+                                                .description("API do projeto Emporium")
+                                                .version("v0.0.1")
+                                                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                                .servers(List.of(
+                                                new Server().url("https://emporium-production.up.railway.app/api/v1")
+                                                                .description("Servidor de Produção"),
+                                                new Server().url("http://localhost:8080/api/v1")
+                                                                .description("Servidor Local")))
+                                .components(new io.swagger.v3.oas.models.Components()
+                                                .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                                                .type(SecurityScheme.Type.HTTP)
+                                                                .scheme("bearer")
+                                                                .bearerFormat("JWT")))
+                                .externalDocs(new ExternalDocumentation()
+                                                .description("Repositório do projeto")
+                                                .url("https://github.com/gabrieudev/emporium/"));
+        }
 }
