5dcfb65c904569cbeabf59d5aaa6163ca52268e0
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-01-19 12:07:41 -0300

    first commit

---
+++ src/main/java/br/com/gabrieudev/emporium/EmporiumApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieudev.emporium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmporiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmporiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrieudev/emporium/EmporiumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieudev.emporium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmporiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
